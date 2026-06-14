f663db677bd89a2558fb99e2575a96916e63f9bd
Prajol <prajolghimire10023@gmail.com>
2026-06-14 17:38:11 +0545

    fix: close missing formGrid div in SuperAdmin form

--- frontend/src/pages/superadmin/SuperAdminDashboard.jsx
+++ frontend/src/pages/superadmin/SuperAdminDashboard.jsx
@@ -479,6 +479,7 @@ export default function SuperAdminDashboard() {
                       Cancel
                     </button>
                   </div>
+                </div>
                 </form>
               </div>
             )}
